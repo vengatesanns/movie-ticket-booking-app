21cea5d2032bbf6c728be309bda13ccb9b2297fb
vengatesanns <vengat19amd@gmail.com>
2020-04-21 00:39:40 +0530

    Response resfactored and unwanted files deleted

--- microservices/libraries/exception-lib/src/main/java/com/showtime/exception/handler/GlobalExceptionHandler.java
+++ microservices/libraries/exception-lib/src/main/java/com/showtime/exception/handler/GlobalExceptionHandler.java
@@ -1,52 +1,47 @@
 package com.showtime.exception.handler;
 
-import java.time.LocalDateTime;
-
-import javax.servlet.http.HttpServletRequest;
-
+import com.showtime.exception.IAMAuthenticationException;
+import com.showtime.exception.UserAlreadyExistsException;
+import com.showtime.exception.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.showtime.exception.IAMAuthenticationException;
-import com.showtime.exception.UserAlreadyExistsException;
-import com.showtime.exception.response.ErrorResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 /**
- * 
  * @author Vengatesan Nagarajan
- *
  */
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-	private static final String OWNER = "com.hackpro";
-	private static final String SERVER_ERROR = "Internal Server ";
-	private static final String CONFLICT_ERROR = "Conflict";
+    private static final String OWNER = "com.hackpro";
+    private static final String SERVER_ERROR = "Internal Server ";
+    private static final String CONFLICT_ERROR = "Conflict";
 
-	@ExceptionHandler({ IAMAuthenticationException.class, UsernameNotFoundException.class,
-			UserAlreadyExistsException.class })
-	public ResponseEntity<ErrorResponse> handleAuthenticationException(Exception ex, HttpServletRequest httpRequest) {
-		return new ResponseEntity<>(ErrorResponse.builder()
-				.timeStamp(LocalDateTime.now())
-				.message(ex.getMessage())
-				.error(CONFLICT_ERROR)
-				.owner(OWNER)
-				.path(httpRequest.getRequestURI())
-				.build(), HttpStatus.CONFLICT);
-	}
+    @ExceptionHandler({IAMAuthenticationException.class, UsernameNotFoundException.class,
+            UserAlreadyExistsException.class})
+    public ResponseEntity<ApiResponse> handleAuthenticationException(Exception ex, HttpServletRequest httpRequest) {
+        return new ResponseEntity<>(ApiResponse.builder()
+                .timeStamp(LocalDateTime.now())
+                .message(ex.getMessage())
+                .error(CONFLICT_ERROR)
+                .owner(OWNER)
+                .path(httpRequest.getRequestURI())
+                .build(), HttpStatus.CONFLICT);
+    }
 
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<ErrorResponse> handleAllException(Exception ex, HttpServletRequest httpRequest) {
-		return new ResponseEntity<>(ErrorResponse.builder()
-				.timeStamp(LocalDateTime.now())
-				.message(ex.getMessage())
-				.error(SERVER_ERROR)
-				.owner(OWNER)
-				.path(httpRequest.getRequestURI())
-				.build(), HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse> handleAllException(Exception ex, HttpServletRequest httpRequest) {
+        return new ResponseEntity<>(ApiResponse.builder()
+                .timeStamp(LocalDateTime.now())
+                .message(ex.getMessage())
+                .error(SERVER_ERROR)
+                .owner(OWNER)
+                .path(httpRequest.getRequestURI())
+                .build(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
 }

8fcf67c94d6c58af625b251db70e31fc75ca0cb3
vengatesanns <vengat19amd@gmail.com>
2020-04-22 09:45:22 +0530

    Eureka Server Added

--- microservices/auth-service/src/main/java/com/showtime/authserver/AuthserverApplication.java
+++ microservices/auth-service/src/main/java/com/showtime/authserver/AuthserverApplication.java
@@ -2,12 +2,14 @@ package com.showtime.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author Vengatesan Nagarajan
  */
 @EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthserverApplication {
 
--- microservices/config-server/src/main/java/com/showtime/configserver/ConfigServerApplication.java
+++ microservices/config-server/src/main/java/com/showtime/configserver/ConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.showtime.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 
@@ -9,6 +10,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @author Vengatesan Nagarajan
  */
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
